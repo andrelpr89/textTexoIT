09a53283e8f410e21a07bafbf925ae041d39264a
Andre Luiz Przynyczuk <andreluiz.przynyczuk@gmail.com>
2022-03-31 17:04:31 -0300

    Projeto iniciado

---
+++ src/main/java/com/testTexoIT/TestTexoItApplication.java
@@ -0,0 +1,13 @@
+package com.testTexoIT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTexoItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestTexoItApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/testTexoIT/TestTexoItApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testTexoIT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestTexoItApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
